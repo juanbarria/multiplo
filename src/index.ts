d85325ff279a4377735173c05478d1198e8295fa
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-05-26 11:47:45 -0300

    correcion1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 let btnMultiplo = document.getElementById("btnMultiplo");
+function esMultiplo (numero1 : number, numero2 : number) : boolean {
+  return ((numero1 % numero2) === 0 );
+  };
 btnMultiplo.addEventListener("click", () => {
   let numero1: number = Number(dato1.value);
   let numero2: number = Number(dato2.value);
-  calcular (numero1, numero2);
 
-  function calcular(numero1: number, numero2: number) {
-    if (numero1 % numero2 === 0) {
-      console.log(numero1, "ES número múltiplo de ", numero2);
+  if ( esMultiplo (numero1, numero2) ) {
+    console.log (numero1, "ES número múltiplo de ", numero2);
     } else {
-      if (numero1 % numero2 > 0) {
-        console.log(numero1, "NO es número múltiplo de ", numero2);
-      }
+    console.log (numero1, "NO es número múltiplo de ", numero2);
     }
-  }
 });
